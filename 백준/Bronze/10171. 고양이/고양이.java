312694f4147f2fd6b7a29cd707223f63e0e8864f
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2024-11-14 17:22:37 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\\");
+        System.out.println("    /\\");
+        System.out.print(" )  (");
+        System.out.println(" \')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}

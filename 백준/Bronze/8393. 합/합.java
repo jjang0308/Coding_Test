ca3f029e80b4323569a8f570826b857c4e34ad8c
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2024-11-19 11:57:27 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int result = 0;
+        for (int i = 0; i <=num; i++) {
+            result+=i;
+        }
+       System.out.println(result);
+    }
+}

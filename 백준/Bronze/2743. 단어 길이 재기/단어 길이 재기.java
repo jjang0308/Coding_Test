2559c008a27b8ef438364420de3c5c2e25197588
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-03-05 15:37:21 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[]arr =  scanner.nextLine().toCharArray();
+        System.out.print(arr.length);
+  }
+}

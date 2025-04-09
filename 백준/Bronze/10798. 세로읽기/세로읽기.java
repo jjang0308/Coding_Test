67ca3f9d9a871cc7e2a873442ea625a62879c602
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-04-09 17:33:33 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,28 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 
 public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] lines = new String[5];
-        // 사용자 입력 배열에 저장
         for (int i = 0; i < 5; i++) {
             lines[i] = br.readLine();
         }
-        StringBuilder result = new StringBuilder();
-        int maxLen = 0;
+        int maxLength = 0;
         for (String line : lines) {
-            maxLen = Math.max(maxLen, line.length());
+            maxLength = Math.max(maxLength, line.length());
         }
-        // 각 열별로 문자 조합
-        for (int i = 0; i < maxLen; i++) {
+        for (int i = 0; i < maxLength; i++) {
             for (int j = 0; j < 5; j++) {
-                if (i < lines[j].length()) {
-                    result.append(lines[j].charAt(i));
+                if(i<lines[j].length()){
+                    sb.append(lines[j].charAt(i));
                 }
             }
         }
-        System.out.println(result.toString());
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }

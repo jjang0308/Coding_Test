326c3291b89c5607b604cff3cb5f53a474315349
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-05-27 18:09:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.50 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,33 @@
 import java.util.*;
-
 class Solution {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
+    
+    static int[]dr = {0, 1, 0, -1};
+    static int[]dc = {-1, 0, 1, 0};
+    
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-
-        boolean[][] visited = new boolean[n][m];
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.offer(new Node(0, 0, 1));
+        Queue<int[]>q = new LinkedList<>();
+        boolean [][] visited = new boolean[n][m];
+        
+        q.add(new int[]{0,0,1});
         visited[0][0] = true;
-
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            if (current.x == n - 1 && current.y == m - 1) {
-                return current.distance;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (maps[nx][ny] == 1 && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        queue.offer(new Node(nx, ny, current.distance + 1));
+        
+        while(!q.isEmpty()){
+            int []cur = q.remove();
+            int r = cur[0]; int c = cur[1]; int dist = cur[2];
+            if(r == n-1 && c == m-1) return dist;
+            
+            for(int i = 0; i<4; i++){
+                int nr = r+dr[i]; int nc = c+dc[i];
+                if(nr >=0 && nr < n && nc >= 0 && nc <m && maps[nr][nc] == 1){
+                    if(!visited[nr][nc]){
+                        visited[nr][nc] = true;
+                        q.add(new int[]{nr, nc, dist+1});
                     }
                 }
             }
         }
-
-        return -1;
-    }
-
-    // Node 클래스: 위치 (x, y) 와 현재까지의 거리
-    static class Node {
-        int x, y, distance;
-
-        Node(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-        }
+     return -1;
     }
 }

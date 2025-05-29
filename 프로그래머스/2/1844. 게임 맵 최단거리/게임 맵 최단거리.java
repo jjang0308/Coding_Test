da5b48ad11772494c866293c785e4f67f6fbbb8b
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-05-29 21:07:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.24 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,28 +1,29 @@
 import java.util.*;
 
 public class Solution {
-    static int[]dr = {0, 1, 0,-1};
+    static int[]dr = {0, 1, 0, -1};
     static int[]dc = {-1, 0, 1, 0};
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         Queue<int[]>q = new LinkedList<>();
         boolean[][]visited = new boolean[n][m];
-        q.offer(new int[]{0,0,1});
+        q.add(new int[]{0,0,1});
         visited[0][0] = true;
+        
         while(!q.isEmpty()){
-            int[]cur = q.poll();
+            int []cur = q.poll();
             int r = cur[0];
             int c = cur[1];
             int dist = cur[2];
-            if(r== n-1 && c== m-1) return dist;
-            for(int i=0; i<4; i++){
-                int nr = r + dr[i];
+            if(r==n-1 && c==m-1) return dist;
+            for(int i =0; i<4; i++){
+                int nr = r +dr[i];
                 int nc = c + dc[i];
-                if(nr >= 0 && nr < n && nc >=0 && nc < m && maps[nr][nc] == 1){
+                if(nr>=0 && nr<n && nc>=0 && nc<m && maps[nr][nc]==1){
                     if(!visited[nr][nc]){
-                        q.offer(new int[]{nr, nc , dist+1});
                         visited[nr][nc] = true;
+                        q.add(new int[]{nr,nc,dist+1});
                     }
                 }
             }

d5d6524b6be0d8819f6692813d33b69cb63bf812
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-05-29 21:27:33 +0900

    [level 3] Title: 단어 변환, Time: 0.59 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,39 @@
 import java.util.*;
-class Solution {
+
+public class Solution {
+    class Word{
+        String name;
+        int count;
+        Word(String name, int count){
+            this.name = name;
+            this.count = count;
+        }
+    }
+    
+    int diffCount(String name, String target){
+        int count = 0;
+        for(int i =0; i<name.length(); i++){
+            if(name.charAt(i) != target.charAt(i))
+                count++;
+        }
+        return count;
+    }
+    
     public int solution(String begin, String target, String[] words) {
         Queue<Word>q = new LinkedList<>();
         boolean[]visited = new boolean[words.length];
-        q.add(new Word(0, begin));
+        q.offer(new Word(begin, 0));
         
         while(!q.isEmpty()){
-            Word cur = q.remove();
-            if(cur.word.equals(target)) return cur.count;
-            for(int i = 0; i<words.length; i++){
-                if(!visited[i] && countDiffCount(cur.word, words[i])==1){
-                    visited[i] =true;
-                    q.add(new Word(cur.count+1, words[i]));
+            Word cur = q.poll();
+            if(cur.name.equals(target)) return cur.count;
+            for(int i=0; i<words.length; i++){
+                if(diffCount(cur.name, words[i]) == 1 && !visited[i]){
+                    q.offer(new Word(words[i], cur.count+1));
+                    visited[i] = true;
                 }
             }
-        
         }
-        return 0;    
-    }
-    
-    int countDiffCount(String word, String target){
-        int diffCount = 0;
-        for(int i =0; i<word.length(); i++){
-            if(word.charAt(i) != target.charAt(i)) diffCount++;
-        }
-        return diffCount;
-    }
-    
-    class Word{
-        int count;
-        String word;
-        
-        public Word(int count, String word){
-            this.count = count;
-            this.word = word;
-        }
+        return 0;
     }
 }

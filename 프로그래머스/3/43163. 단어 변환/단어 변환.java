d94770af0d83ffe0c75fad432614da39abfd898c
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-06-27 10:58:43 +0900

    [level 3] Title: 단어 변환, Time: 0.66 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,9 +1,11 @@
 import java.util.*;
 
 public class Solution {
+    
     class Word{
         String name;
         int count;
+        
         Word(String name, int count){
             this.name = name;
             this.count = count;
@@ -12,28 +14,30 @@ public class Solution {
     
     int diffCount(String name, String target){
         int count = 0;
-        for(int i =0; i<name.length(); i++){
-            if(name.charAt(i) != target.charAt(i))
+        for(int i = 0; i<name.length(); i++){
+            if(name.charAt(i) != target.charAt(i)){
                 count++;
+            }
         }
         return count;
     }
     
-    public int solution(String begin, String target, String[] words) {
-        Queue<Word>q = new LinkedList<>();
-        boolean[]visited = new boolean[words.length];
-        q.offer(new Word(begin, 0));
-        
-        while(!q.isEmpty()){
-            Word cur = q.poll();
-            if(cur.name.equals(target)) return cur.count;
-            for(int i=0; i<words.length; i++){
-                if(diffCount(cur.name, words[i]) == 1 && !visited[i]){
-                    q.offer(new Word(words[i], cur.count+1));
-                    visited[i] = true;
-                }
-            }
-        }
-        return 0;
+    
+     public int solution(String begin, String target, String[] words) {
+         Queue<Word>q = new LinkedList<>();
+         boolean[]visited = new boolean[words.length];
+         q.offer(new Word(begin,0));
+         // visited[be] = true;
+         while(!q.isEmpty()){
+             Word cur = q.poll();
+             if(cur.name.equals(target)) return cur.count;
+             for(int i = 0; i<words.length; i++){
+                  if(!visited[i] && diffCount(cur.name, words[i]) == 1){
+                      q.offer(new Word(words[i], cur.count+1));
+                      visited[i] = true;
+                  }
+             }
+         }
+         return 0;
     }
 }

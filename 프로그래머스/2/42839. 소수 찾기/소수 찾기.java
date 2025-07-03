703d9402b13502073108d3a9e8f02520ea0d5329
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-07-03 11:49:01 +0900

    [level 2] Title: 소수 찾기, Time: 122.32 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    Set<Integer>primeSet;
+    public int solution(String numbers) {
+        int answer = 0;
+        primeSet = new HashSet<>();
+        boolean[]visited = new boolean[numbers.length()];
+        backtrack(numbers , visited, "");
+        return primeSet.size();
+    }
+    
+    public void backtrack(String numbers, boolean[]visited, String cur){
+        if(cur.length() !=0){
+            int num = Integer.parseInt(cur);
+            if(isPrime(num)) primeSet.add(num);
+        }
+        for(int i = 0; i< numbers.length(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                backtrack(numbers,visited ,cur+numbers.charAt(i));
+                visited[i]=false;
+            }
+           
+        }
+    }
+    
+    public boolean isPrime(int num){
+        if(num < 2) return false;
+        for(int i =2; i<num; i++){
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
+}

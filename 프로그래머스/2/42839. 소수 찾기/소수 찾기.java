b4d80046f0792103dcabe6db4b8cfe37b2d57c09
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-07-04 13:16:41 +0900

    [level 2] Title: 소수 찾기, Time: 107.59 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,33 +1,30 @@
 import java.util.*;
 class Solution {
-    Set<Integer>primeSet;
+    Set<Integer> primeSet;
     public int solution(String numbers) {
-        int answer = 0;
         primeSet = new HashSet<>();
         boolean[]visited = new boolean[numbers.length()];
-        backtrack(numbers , visited, "");
+        dfs(numbers, visited, "");
         return primeSet.size();
     }
     
-    public void backtrack(String numbers, boolean[]visited, String cur){
-        if(cur.length() !=0){
-            int num = Integer.parseInt(cur);
-            if(isPrime(num)) primeSet.add(num);
+    void dfs(String numbers, boolean[]visited, String cur){
+        if(cur.length() != 0){
+            if(isPrime(Integer.parseInt(cur))) primeSet.add(Integer.parseInt(cur));
         }
-        for(int i = 0; i< numbers.length(); i++){
+        for(int i = 0; i<numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                backtrack(numbers,visited ,cur+numbers.charAt(i));
-                visited[i]=false;
+                dfs(numbers, visited, cur + numbers.charAt(i));
+                visited[i] = false;
             }
-           
         }
     }
     
-    public boolean isPrime(int num){
+    boolean isPrime(int num){
         if(num < 2) return false;
         for(int i =2; i<num; i++){
-            if(num%i == 0) return false;
+            if(num % i == 0) return false;
         }
         return true;
     }

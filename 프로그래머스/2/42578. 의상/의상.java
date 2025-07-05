5d9962f10b9020cf01db28f96d27b908ca60d828
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-07-06 01:21:21 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,11 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        for(String[]s : clothes){
-            for(String c: s){
-                System.out.println(c);
-            }
+        Map<String, Integer>map = new HashMap<>();
+        for(String[]c : clothes){
+            map.put(c[1] , map.getOrDefault(c[1], 0) + 1);
         }
-        return 5;
+        int answer = 1;
+        // headgear : 2 + 아무것도 안 입음 1 = 3
+        // eyewear : 1 + 아무것도 안 입음 1 = 2
+        // 아무것도 안 입음 , 아무것도 안 입음 -> x 이므로 answer - 1
+        
+        for(int count : map.values()){
+            answer *= (count + 1);
+        }
+        return answer -1;
     }
 }
+

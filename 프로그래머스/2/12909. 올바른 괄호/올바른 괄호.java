0744c43615814ac8ea133b844cddfeb4dd9932be
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-09-15 23:10:37 +0900

    [level 2] Title: 올바른 괄호, Time: 20.37 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,24 +2,25 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
         Stack<Character>stack = new Stack<>();
         for(char c : s.toCharArray()){
-           if(stack.isEmpty()){
-               if(c == '('){
-                   stack.push(c);
-               }else{
-                   return false;
-               }
-           }else{
-               if(c == '('){
-                   stack.push(c);
-               }else{
-                   stack.pop();
-               }
-           }
+            if(stack.isEmpty()){
+                if(c == '('){
+                    stack.push('(');
+                }else{
+                    return false;
+                }   
+            }else{
+                if(c == '('){
+                    stack.push('(');
+                }else{
+                    stack.pop();
+                }
+            }
         }
-        if(!stack.isEmpty())
-            return false;
-        return true;
+        if(!stack.isEmpty()) return false;
+
+        return answer;
     }
 }

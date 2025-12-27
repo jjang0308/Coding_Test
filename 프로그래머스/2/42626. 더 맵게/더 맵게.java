f52fa560e94188cd490e1c483743847531e7965a
tadpole_in_the_well <127084579+jjang0308@users.noreply.github.com>
2025-12-27 17:42:42 +0900

    [level 2] Title: 더 맵게, Time: 1616.69 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,24 +1,24 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int s : scoville) {
+        int answer = 0;
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        
+        for(int s : scoville){
             pq.offer(s);
         }
-
-        int answer = 0;
-
-        while (pq.size() > 1 && pq.peek() < K) {
+        
+        while(pq.size() > 1 && pq.peek() < K){
             int a = pq.poll();
             int b = pq.poll();
             int c = a + (b * 2);
             pq.offer(c);
             answer++;
         }
-
-        if (pq.peek() < K) return -1;
-
+        
+        if(pq.peek() < K) return -1;
+            
         return answer;
     }
 }
